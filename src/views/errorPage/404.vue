ddf386c445a20885a616dba30cfc0dcba2c5c815
zqg <451391872@qq.com>
2023-05-04 19:13:09 +0800

    feat: 修改错误页面图片和样式

--- src/views/errorPage/404.vue
+++ src/views/errorPage/404.vue
@@ -1,7 +1,28 @@
 <template>
-  <h1>hello 404</h1>
+  <div class="not-container">
+    <img src="@/assets/images/404.png" class="not-img" alt="404" />
+    <div class="not-detail">
+      <h2>404</h2>
+      <h4>抱歉，您访问的页面不存在~</h4>
+      <el-button type="primary" @click="goBack()">返回</el-button>
+      <el-button class="home-icon" link type="primary" @click="goHome()">返回首页</el-button>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
 
-<style lang="scss" scoped></style>
+const router = useRouter()
+
+const goBack = () => {
+  router.back()
+}
+const goHome = () => {
+  router.replace({ path: '/home/index' })
+}
+</script>
+
+<style scoped lang="scss">
+@import './index.scss';
+</style>
