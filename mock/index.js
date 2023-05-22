3895301b7dc17a09c9c71c2fc77b110fc644335f
zb.zhu <qqq980@163.com>
2023-05-22 19:58:15 +0800

    mockjs入口文件批量引入api模块实现

--- mock/index.js
+++ mock/index.js
@@ -1,1 +1,9 @@
-import './api/caseRecord.js'
+//批量引入api模块
+const modulesFiles = import.meta.globEager('./api/*.js')
+
+export default Object.keys(modulesFiles).reduce((modules, modulePath) => {
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles[modulePath]
+  modules[moduleName] = value.default
+  return modules
+}, {})
