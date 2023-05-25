1e856c7fdac9e1f68ae2be1f6f6047b30378e311
zb.zhu <qqq980@163.com>
2023-05-25 17:30:21 +0800

    添加委案预收回页及弹窗配置

---
+++ src/views/caseCirculation/delegatedCaseManage/caseAheadRecovery/config/tableColumnList.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    label: '收回机构',
+    prop: 'caseCategory',
+    width: 100
+  },
+  {
+    label: '收回备注',
+    prop: 'caseProduct',
+    width: 200
+  },
+  {
+    label: '操作维度',
+    prop: 'operaNum'
+  },
+  {
+    label: '执行时间',
+    prop: 'orderNo'
+  },
+  {
+    label: '预计收回金额',
+    prop: 'orderNo'
+  },
+  {
+    label: '预计收回案件量',
+    prop: 'orderNo'
+  },
+  {
+    label: '预计收回案人数',
+    prop: 'orderNo'
+  },
+  {
+    label: '操作人',
+    prop: 'orderNo'
+  },
+  {
+    label: '操作时间',
+    prop: 'orderNo'
+  }
+]
