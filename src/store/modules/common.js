323db5ce773cf5c3ba2eca60b9dd57bb98ed4aab
zqg <451391872@qq.com>
2023-05-29 17:58:57 +0800

    feat: 删除冗余代码

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -3,14 +3,11 @@ import Apis from '@/api/modules/common'
 
 export const useCommonStore = defineStore('commonStore', {
   state: () => ({
-    deptTree: [], // 部门
-    positionList: [], // 职位
-    roleList: [], // 角色
     dropdownList: {
-      COOPERATION_STATUS: [],
-      ROLE: [],
-      DEPT: [],
-      POSITION: []
+      COOPERATION_STATUS: [], // 机构合作状态
+      ROLE: [], // 角色列表
+      DEPT: [], // 部门列表
+      POSITION: [] // 职位列表
     }
   }),
   actions: {
