415f8368841ab3d4ecaa9b40ac604597d6c57f96
zqg <451391872@qq.com>
2023-06-14 16:23:15 +0800

    feat: 去除冗余代码

--- src/api/modules/common.js
+++ src/api/modules/common.js
@@ -6,8 +6,6 @@ const apiConfig = {
   findItemList: `GET /upms/client/dict/item/list`, // 查询字典项
   // 省市区
   areaTree: `GET /upms/client/dict/area/tree`,
-  // 字典项 DEPT_LIST-部门列表，ROLE_LIST-角色列表，PERMISSION_TEMPLATE_LIST-权限模版列表，TENANT_LIST-租户列表
-  dictDropdownList: `GET /upms/admin/dict/dropdownList`
 }
 
 const apis = generateApiFnc(apiConfig)
