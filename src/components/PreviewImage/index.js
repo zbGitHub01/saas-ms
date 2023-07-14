bb47d236313d46cb56fb6fefb2dd5d7ff9660508
zqg <451391872@qq.com>
2023-07-14 11:00:46 +0800

    feat: 封装图片预览方法添加注释

--- src/components/PreviewImage/index.js
+++ src/components/PreviewImage/index.js
@@ -14,6 +14,12 @@ const createPreviewImage = options => {
   render(vnode, container)
   document.body.appendChild(container.firstElementChild)
 }
+/**
+ * @description: 图片预览方法
+ * @param urlList 图片地址，多张图片必须是数组，一张图片可以是字符串
+ * @param index 图片预览展示的下标，不传默认为0
+ * @return void
+ */
 export const previewImage = (urlList, index = 0) => {
   if (typeof urlList === 'string') {
     urlList = [urlList]
